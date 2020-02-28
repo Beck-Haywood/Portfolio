66e7e288407c29750729fc4a56c6efcebc138609
Beck-Haywood <beck.haywood@students.makeschool.com>
2020-02-28 10:46:21 -0800

    github live

--- portfolio/myjs.js
+++ portfolio/myjs.js
@@ -1,19 +1,14 @@
 //my Scripts 
         
 const card1 = document.getElementById('c1')
-const card2 = document.getElementById('c2')
 const card3 = document.getElementById('c3')
-const card4 = document.getElementById('c4')
 
 const button1 = document.getElementById('button1')
 const button2 = document.getElementById('button2')
 const button3 = document.getElementById('button3')
-const button4 = document.getElementById('button4')
 
 const close1 = document.getElementById('close1')
-const close2 = document.getElementById('close2')
 const close3 = document.getElementById('close3')
-const close4 = document.getElementById('close4')
 
 const cont = document.getElementById('cont')
 
@@ -56,15 +51,6 @@ button1.addEventListener('click', function(e) {
     lessTheOppacity("hide1")
     lessTheOppacity("hide2")
     lessTheOppacity("hide3")
-    lessTheOppacity("hide4")
-})
-button2.addEventListener('click', function(e) {
-    card2.classList.toggle('flip')
-    cont.classList.toggle('clickclose')
-    lessTheOppacity("hide1")
-    lessTheOppacity("hide2")
-    lessTheOppacity("hide3")
-    lessTheOppacity("hide4")
 })
 button3.addEventListener('click', function(e) {
     card3.classList.toggle('flip')
@@ -72,7 +58,6 @@ button3.addEventListener('click', function(e) {
     lessTheOppacity("hide1")
     lessTheOppacity("hide2")
     lessTheOppacity("hide3")
-    lessTheOppacity("hide4")
 })
 
 close1.addEventListener('click', function(e) {
@@ -81,15 +66,6 @@ close1.addEventListener('click', function(e) {
     increaseTheOppacity("hide1")
     increaseTheOppacity("hide2")
     increaseTheOppacity("hide3")
-    increaseTheOppacity("hide4")
-})
-close2.addEventListener('click', function(e) {
-    card2.classList.toggle('flip')
-    cont.classList.toggle('clickclose')
-    increaseTheOppacity("hide1")
-    increaseTheOppacity("hide2")
-    increaseTheOppacity("hide3")
-    increaseTheOppacity("hide4")
 })
 close3.addEventListener('click', function(e) {
     card3.classList.toggle('flip')
@@ -97,5 +73,4 @@ close3.addEventListener('click', function(e) {
     increaseTheOppacity("hide1")
     increaseTheOppacity("hide2")
     increaseTheOppacity("hide3")
-    increaseTheOppacity("hide4")
 })
